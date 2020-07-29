0ffc0e3c2eec4f71624a902c969f596712e690cc
Giovani Guichard <giovanimguichard@gmail.com>
2020-07-29 01:37:03 -0300

    refinamentos

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,5 +1,20 @@
 function mudaFoto(foto) {
-    document.getElementById("icone").src = foto;
+    document.getElementById('icone').src = foto
 }
 
-console.log("oi")
+window.addEventListener('load', () => {
+    const hideAll = () =>
+        janelas.forEach((janela) => janela.classList.remove('show'))
+    const showById = (id) => document.getElementById(id).classList.add('show')
+    const onClick = (ev) => {
+        ev.preventDefault()
+        hideAll()
+        const el = ev.target
+        const id = el.target
+        showById(id)
+    }
+
+    const areas = document.querySelectorAll('map area')
+    const janelas = document.querySelectorAll('.janela')
+    areas.forEach((area) => area.addEventListener('click', onClick))
+})
